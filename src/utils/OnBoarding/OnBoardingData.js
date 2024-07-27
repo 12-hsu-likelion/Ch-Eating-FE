7bc7307390c2ca8c04a9f154442e6d9a5ebfbcd6
sheepyis <zx9086@naver.com>
2024-07-28 02:25:57 +0900

    ✨onBoarding

---
+++ src/utils/OnBoarding/OnBoardingData.js
@@ -0,0 +1,28 @@
+const OnBoardingData = [
+    { 
+        "id": 1, 
+        "imageUrl": "onBoarding1.png",
+        "title": "가짜 배고픔 테스트", 
+        "subTitle": "테스트를 통해.가짜 배고픔과 진짜 배고픔을 구별하세요" 
+    },
+    { 
+        "id": 2, 
+        "imageUrl": "onBoarding2.png",
+        "title": "테스트 결과 확인", 
+        "subTitle": "테스트 결과는 자동 입력되고.통계 결과를 통해 나의 변화를 확인할 수 있어요" 
+    },
+    { 
+        "id": 3, 
+        "imageUrl": "onBoarding3.png",
+        "title": "알림 서비스", 
+        "subTitle": "내가 허기를 자주 느꼈던 시간에.알림을 받을 수 있어요" 
+    },
+    { 
+        "id": 4, 
+        "imageUrl": "onBoarding4.png",
+        "title": "식사량 기록", 
+        "subTitle": "내가 먹을 수 있는 양을 기록해서.과식을 방지하세요" 
+    }
+];
+
+export default OnBoardingData;
