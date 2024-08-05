be65257abf73f2690a7f63161098ed52e259bf48
sheepyis <zx9086@naver.com>
2024-08-05 17:07:33 +0900

    🚑 onBoarding_재수정

--- src/utils/OnBoarding/OnBoardingData.js
+++ src/utils/OnBoarding/OnBoardingData.js
@@ -8,20 +8,20 @@ const OnBoardingData = [
     { 
         "id": 2, 
         "imageUrl": "onBoarding2.png",
-        "title": "테스트 결과 확인", 
-        "subTitle": "테스트 결과는 자동 입력되고.통계 결과를 통해 나의 변화를 확인할 수 있어요" 
-    },
-    { 
-        "id": 3, 
-        "imageUrl": "onBoarding3.png",
         "title": "테스트 결과 관리", 
         "subTitle": "테스트 결과는.자동으로 캘린더에 기록돼요" 
     },
     { 
-        "id": 4, 
-        "imageUrl": "onBoarding4.png",
+        "id": 3, 
+        "imageUrl": "onBoarding3.png",
         "title": "배고픔 통계", 
         "subTitle": "통계를 통해 가짜 배고픔 패턴을 파악하고.변화를 확인할 수 있어요" 
+    },
+    { 
+        "id": 4, 
+        "imageUrl": "onBoarding4.png",
+        "title": "식사량 기록", 
+        "subTitle": "내가 먹을 수 있는 양을 기록해서.과식을 방지하세요" 
     }
 ];
 
